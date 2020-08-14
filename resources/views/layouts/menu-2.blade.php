ed9f403620b7126fb967e837782b75e54ca26789
Angelo Meneses <angelo.jm2202@gmail.com>
2020-08-13 20:24:49 -0400

    actualizacion

--- resources/views/layouts/menu-2.blade.php
+++ resources/views/layouts/menu-2.blade.php
@@ -41,107 +41,108 @@
                                             <a href="{{route('PrimerEquipo')}}">Plantilla</a>
                                         </li>
                                         <li>
+                                        <li>
                                             <a href="{{route('home.filtrar.categoria', 'Primer Equipo')}}">Noticias</a>
                                         </li>
-
-                                    </ul>
                                 </li>
 
-                                <li class="current">
-                                    <a href="#">Juveniles</a>
-                                    <ul class="sub-current">
-                                        <li>
-                                            <a href="{{route('sub20')}}">Sub20</a>
-                                        </li>
-                                        <li>
-                                            <a href="{{route('sub19')}}">Sub19</a>
-                                        </li>
-                                        <li>
-                                            <a href="{{route('sub18')}}">Sub18</a>
-                                        </li>
-                                        <li>
-                                            <a href="{{route('home.filtrar.categoria', 'Juveniles')}}">Noticias</a>
-                                        </li>
-                                    </ul>
-                                </li>
+                            </ul>
+                            </li>
 
-                                <li class="current logo">
-                                    <!-- Logo-->
-                                    <a href="{{url ('/')}}" title="Return Home">
-                                        <img src="{{asset('img/logo.png')}}" alt="Logo" class="logo_img">
-                                    </a>
-                                    <!-- End Logo-->
-                                </li>
+                            <li class="current">
+                                <a href="#">Juveniles</a>
+                                <ul class="sub-current">
+                                    <li>
+                                        <a href="{{route('sub20')}}">Sub20</a>
+                                    </li>
+                                    <li>
+                                        <a href="{{route('sub19')}}">Sub19</a>
+                                    </li>
+                                    <li>
+                                        <a href="{{route('sub18')}}">Sub18</a>
+                                    </li>
+                                    <li>
+                                        <a href="{{route('home.filtrar.categoria', 'Juveniles')}}">Noticias</a>
+                                    </li>
+                                </ul>
+                            </li>
 
-                                <li class="current">
-                                    <a href="#">Fútbol Base</a>
-                                    <ul class="sub-current">
-                                        <li>
-                                            <a href="{{url('futbase')}}">Sub16</a>
-                                        </li>
-                                        <li>
-                                            <a href="{{url('futbase')}}">Sub14</a>
-                                        </li>
-                                        <li>
-                                            <a href="{{route('home.filtrar.categoria', 'Fútbol Base')}}">Noticias</a>
-                                        </li>
-                                    </ul>
-                                </li>
+                            <li class="current">
+                                <a href="#">Fútbol Base</a>
+                                <ul class="sub-current">
+                                    <li>
+                                        <a href="{{url('futbase')}}">Sub16</a>
+                                    </li>
+                                    <li>
+                                        <a href="{{url('futbase')}}">Sub14</a>
+                                    </li>
+                                    <li>
+                                        <a href="{{route('home.filtrar.categoria', 'Fútbol Base')}}">Noticias</a>
+                                    </li>
+                                </ul>
+                            </li>
 
-                                <li>
-                                    <a href="#">Campeonatos</a>
-                                    <div class="sf-mega">
-                                        <div class="row">
-                                            <div class="col-md-3">
-                                                <h5><i class="fa fa-trophy" aria-hidden="true"></i>Liga</h5>
-                                                <ul>
-                                                    <li><a href="#">Tabla de Posiciones</a></li>
-                                                    <li><a href="#">Resultados</a></li>
-                                                    <li><a href="#">Grupos</a></li>
-                                                    <li><a href="#">Noticias</a></li>
-                                                    <li><a href="#">FVF</a></li>
-                                                </ul>
+                            <li class="current logo">
+                                <!-- Logo-->
+                                <a href="{{url ('/')}}" title="Return Home">
+                                    <img src="{{asset('img/logo.png')}}" alt="Logo" class="logo_img">
+                                </a>
+                                <!-- End Logo-->
+                            </li>
+
+                            <li>
+                                <a href="#">Campeonatos</a>
+                                <div class="sf-mega">
+                                    <div class="row">
+                                        <div class="col-md-3">
+                                            <h5><i class="fa fa-trophy" aria-hidden="true"></i>Liga</h5>
+                                            <ul>
+                                                <li><a href="#">Tabla de Posiciones</a></li>
+                                                <li><a href="#">Resultados</a></li>
+                                                <li><a href="#">Grupos</a></li>
+                                                <li><a href="#">Noticias</a></li>
+                                                <li><a href="https://www.federacionvenezolanadefutbol.org/">FVF</a></li>
+                                            </ul>
+                                        </div>
+
+                                        <div class="col-md-3">
+                                            <h5><i class="fa fa-calendar" aria-hidden="true"></i> Calendario</h5>
+                                            <div class="img-hover">
+                                                <img src="{{asset('img/blog/1.jpg')}}" alt="imagen equipo" class="img-responsive">
+                                                <div class="overlay"><a href="#">+</a></div>
                                             </div>
+                                        </div>
 
-                                            <div class="col-md-3">
-                                                <h5><i class="fa fa-users" aria-hidden="true"></i> Equipos</h5>
-                                                <div class="img-hover">
-                                                    <img src="{{asset('img/blog/1.jpg')}}" alt="" class="img-responsive">
-                                                    <div class="overlay"><a href="#">+</a></div>
-                                                </div>
+                                        <div class="col-md-3">
+                                            <h5><i class="fa fa-futbol-o" aria-hidden="true"></i> Jugadores</h5>
+                                            <div class="img-hover">
+                                                <img src="{{asset('img/blog/2.jpg')}}" alt="imagen jugadores" class="img-responsive">
+                                                <div class="overlay"><a href="#">+</a></div>
                                             </div>
+                                        </div>
 
-                                            <div class="col-md-3">
-                                                <h5><i class="fa fa-futbol-o" aria-hidden="true"></i> Jugadores</h5>
-                                                <div class="img-hover">
-                                                    <img src="{{asset('img/blog/2.jpg')}}" alt="" class="img-responsive">
-                                                    <div class="overlay"><a href="#">+</a></div>
-                                                </div>
-                                            </div>
-
-                                            <div class="col-md-3">
-                                                <h5><i class="fa fa-gamepad" aria-hidden="true"></i> Resultados</h5>
-                                                <div class="img-hover">
-                                                    <img src="{{asset('img/blog/3.jpg')}}" alt="" class="img-responsive">
-                                                    <div class="overlay"><a href="#">+</a></div>
-                                                </div>
+                                        <div class="col-md-3">
+                                            <h5><i class="fa fa-gamepad" aria-hidden="true"></i> Resultados</h5>
+                                            <div class="img-hover">
+                                                <img src="{{asset('img/blog/3.jpg')}}" alt="imagen resultados" class="img-responsive">
+                                                <div class="overlay"><a href="#">+</a></div>
                                             </div>
                                         </div>
                                     </div>
-                                </li>
+                                </div>
+                            </li>
 
-                                <!-- <li class="current">
-                                    <a href="#">Atletas</a>
-                                    <ul class="sub-current">
-                                        <li>
-                                            <a href="#">Lista Jugadores</a>
-                                        </li>
-                                    </ul>
-                                </li> -->
+                            <li class="current">
+                                <a href="{{ url('galeria') }}">Multimedia</a>
+                            </li>
 
-                                <li>
-                                    <a href="{{ url('contacto') }}">Contacto</a>
-                                </li>
+                            <li class="current">
+                                <a href="#">Calendario</a>
+                            </li>
+
+                            <li class="current">
+                                <a href="{{ url('contacto') }}">Contacto</a>
+                            </li>
                             </ul>
                             <!-- End Menu-->
                         </div>
